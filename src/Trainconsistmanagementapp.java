c5c192a8090b6d2e9d24bb1c184fde069d05c0db
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-03-20 09:18:16 +0530

    UC1: Initialize train and display consist summary

--- src/Trainconsistmanagementapp.java
+++ src/Trainconsistmanagementapp.java
@@ -1,2 +1,9 @@
-public class Trainconsistmanagementapp {
+import java.util.*;
+public class Trainconsistmanagmentapp
+    public static void main(String[] args) {
+        System.out.println("git checkout develop\n" +
+                "git checkout -b feature/UC1 Train Consist Management App ");
+        List<String> bogies = new ArrayList<>();
+        System.out.println("Initial Bogie Count: " + bogies.size());
+    }
 }
