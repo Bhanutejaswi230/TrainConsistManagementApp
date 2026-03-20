5285ba6e822776fc89a2908fc151338f2193af70
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-03-20 09:41:19 +0530

    UC2: ArrayList operations for bogies

--- src/Trainconsistmanagementapp.java
+++ src/Trainconsistmanagementapp.java
@@ -1,9 +1,14 @@
 import java.util.*;
-public class Trainconsistmanagmentapp
+public class Trainconsistmanagementapp {
     public static void main(String[] args) {
-        System.out.println("git checkout develop\n" +
-                "git checkout -b feature/UC1 Train Consist Management App ");
+        System.out.println(" Train Consist Management App ");
         List<String> bogies = new ArrayList<>();
-        System.out.println("Initial Bogie Count: " + bogies.size());
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+        System.out.println("Bogies after adding: " + bogies);
+        bogies.remove("AC Chair");
+        System.out.println("After removing AC Chair: " + bogies);
+        System.out.println("Is Sleeper present? " + bogies.contains("Sleeper"));
     }
 }
